3fc4668318d96f1db35c34862f0b6b4b68f0f884
Marius Gächter <marius.gaechter@leafit.ch>
2013-07-04 14:37:48 +0200

    First Commit

---
+++ BiluneGeoMobile/BiluneGeoMobile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BiluneGeoMobile
+//
+//  Created by Marius Gächter on 04.07.13.
+//  Copyright (c) 2013 leafit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
